221505075f2b090047e413ab8bc6fed5bb801644
jamessli <jamesshuyueli@gmail.com>
2022-06-03 14:34:45 -0700

    Delete setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='ECHO_LVH_PACKAGE',
-    install_requires=['pandas','scikit-learn', 'tensorflow', 'numpy', 'matplotlib.cm', 'cv2', 'natsort', 'pathlib', 'isort', 'streamlit', 'IPython.display', 'pickle', 'time'],
-    packages=find_packages()
-)
